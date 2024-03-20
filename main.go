1b4a6f677e749e865b43387178f24ecd2a51f417
patriciacostadacruz <patriciacstcz@gmail.com>
2024-03-20 18:38:20 +0100

    Adds Colly logic to start scrapping idealista

--- main.go
+++ main.go
@@ -1,9 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/gocolly/colly/v2"
+) 
 
 func main() {
-	sentance := "Hey, it's empty still, code in construction..."
-	fmt.Println(sentance)
+	fmt.Println("Executing main.go...")
+
+	scrapper := colly.NewCollector()
+	var url_to_scrap = "https://es.wikipedia.org/wiki/Go_(lenguaje_de_programaci%C3%B3n)"
+
+	scrapper.OnRequest(func(r *colly.Request) {
+		fmt.Println("Requesting page: ", r.URL.String())
+	})
+
+	scrapper.OnResponse(func(r *colly.Response) {
+		fmt.Println("Got a response from page: ", r.Request.URL)
+	})
+
+	scrapper.OnError(func(_ * colly.Response, e error) {
+		fmt.Println("An error occurred: ", e)
+	})
+
+	scrapper.OnHTML("main", func(e *colly.HTMLElement) {
+		articleName := e.ChildText("span.mw-page-title-main")
+		articleName = strings.TrimSpace(articleName)
+
+		fmt.Println("Scrapped article title: ", articleName)
+	})
+
+	err := scrapper.Visit(url_to_scrap)
+	
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
