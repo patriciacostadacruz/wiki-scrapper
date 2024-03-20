465d07101afc59450c1ed62cb5348487f4234b84
patriciacostadacruz <patriciacstcz@gmail.com>
2024-03-20 18:57:12 +0100

    Adds a few more steps as saving data to JSON file

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Wiki-scrapper
 
 This is a repository created to practice scrapping data with Go & Colly.
+
+## Execution
+
+Beforehand, make sure to install the necessary dependencies:
+`go mod tidy`
+
+To execute the program, run: `go run main.go`
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"os"
 	"strings"
 
 	"github.com/gocolly/colly/v2"
@@ -10,33 +12,43 @@ import (
 
 func main() {
 	fmt.Println("Executing main.go...")
+	var articleName string
 
 	scrapper := colly.NewCollector()
+	
 	var url_to_scrap = "https://es.wikipedia.org/wiki/Go_(lenguaje_de_programaci%C3%B3n)"
 
 	scrapper.OnRequest(func(r *colly.Request) {
 		fmt.Println("Requesting page: ", r.URL.String())
+		// r.Headers.Set("User-Agent", "to_be_created")
 	})
 
 	scrapper.OnResponse(func(r *colly.Response) {
 		fmt.Println("Got a response from page: ", r.Request.URL)
 	})
 
-	scrapper.OnError(func(_ * colly.Response, e error) {
-		fmt.Println("An error occurred: ", e)
-	})
-
-	scrapper.OnHTML("main", func(e *colly.HTMLElement) {
-		articleName := e.ChildText("span.mw-page-title-main")
-		articleName = strings.TrimSpace(articleName)
-
-		fmt.Println("Scrapped article title: ", articleName)
-	})
-
-	err := scrapper.Visit(url_to_scrap)
 	
-	if err != nil {
-		log.Fatal(err)
-	}
+	scrapper.OnHTML("main", func(e *colly.HTMLElement) {
+		articleName = e.ChildText("span.mw-page-title-main")
+		articleName = strings.TrimSpace(articleName)
+	})
+	
+	scrapper.OnError(func(r * colly.Response, e error) {
+		log.Fatal("An error occurred: ", e)
+	})
+
+	scrapper.OnScraped(func(r *colly.Response) {
+		fmt.Println("Scrapped article title: ", articleName)
+		js, err := json.MarshalIndent(articleName, "", "    ")
+      if err != nil {
+        log.Fatal(err)
+      }
+      fmt.Println("Writing data to file...")
+      if err := os.WriteFile("article-data.json", js, 0664); err == nil {
+          fmt.Println("Data written to file successfully.")
+      }
+	})
+
+	scrapper.Visit(url_to_scrap)
 }
 
